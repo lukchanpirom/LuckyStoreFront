bb1402dcde456c41137ca2b89407fb82643280d6
lukchanpirom <luk.cpr@gmail.com>
2020-10-23 19:00:26 +0700

    v2.1

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/home/Home.vue'
+import Home from '../views/Home.vue'
 import Store from '../views/Store.vue'
 
 Vue.use(VueRouter)
